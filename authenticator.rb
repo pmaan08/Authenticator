86dff685dab92c62fdfa9e94d45c3388e59a1597
pmann08 <partulmann@gmail.com>
2022-09-20 20:21:08 +0530

    final commit

--- authenticator.rb
+++ authenticator.rb
@@ -1,1 +1,31 @@
-puts "Hello World"
+include db
+
+#authentication method, to check if password & username match
+def auth_user(username, password, user_db)
+    user_db.each do |user_detail|
+        if user_detail[:username] == username && user_detail[:password] == password
+            return user_detail
+        end
+    end
+    "Wrong credentials"
+end
+
+puts " welcome to AUTHENTICATOR"
+30.times {print "-"}
+puts 
+puts "On entering correct password, user details will be returned"
+attempts = 1
+while attempts <= 3
+    print "Username:"
+    username = gets.chomp
+    print "Password:"
+    password = gets.chomp
+    authentication = auth_user(username, password, users)
+    puts authentication
+    puts " Press z to exit or any key to continue"
+    input = gets.chomp
+    break if input == "z"
+    attempts +=1
+    puts " Number of attemots left #{attempts}"
+end
+puts " Number of attempts are over" if attempts == 4  
---
+++ db.rb
@@ -0,0 +1,6 @@
+users = [
+    {username: "partul", password: "123"},
+    {username: "partul1", password: "1234"},
+    {username: "partul2", password: "12345"},
+    {username: "partul3", password: "123456"}
+]
